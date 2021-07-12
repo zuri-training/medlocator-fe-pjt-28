03d8ca12af25d07bb868335faf2406cc2bacf6e3
Dipo-Jaiye <durojaiyep@yahoo.co.uk>
2021-07-12 08:29:21 +0100

    Add pseudo authentication

--- assets/pharmacy_main_screen/main_screen.js
+++ assets/pharmacy_main_screen/main_screen.js
@@ -1,11 +1,18 @@
 window.onload = () => {
-const pharm_details = document.getElementById("pharmacy_details");
 const store = JSON.parse(sessionStorage.getItem("store"));
-const {name, contact} = store;
-if(name && contact){
-    pharm_details.innerHTML = `${name} <br> ${contact.phone}`;
+if(!store){
+    alert("Login required!");
+    document.location = "login.html";
 }
 else{
+    const pharm_details = document.getElementById("pharmacy_details");
+    const {name, contact} = store;
+    if(name && contact){
+    pharm_details.innerHTML = `${name} <br> ${contact.phone}`;
+    }
+    else{
     pharm_details.innerHTML = name;
+    }
 }
+
 };
